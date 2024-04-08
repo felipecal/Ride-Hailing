f77944b5cfb44983465f6c79e7e4f322af3e4257
Felipe <fcalldas@gmail.com>
2024-04-08 20:24:49 -0300

    ✅ test: add a new test

--- test/api.test.ts
+++ test/api.test.ts
@@ -4,7 +4,7 @@ axios.defaults.validateStatus = function () {
 	return true;
 }
 
-test("Deve criar uma conta para o passageiro", async function () {
+test("Deve criar uma conta para o passageiro e consultar conta", async function () {
 	const input = {
 		name: "John Doe",
 		email: `john.doe${Math.random()}@gmail.com`,
@@ -14,4 +14,24 @@ test("Deve criar uma conta para o passageiro", async function () {
 	const resultSignUp = await axios.post("http://localhost:3000/signup", input);
 	const resultOfGetUser = await axios.get(`http://localhost:3000/getAccountById/${resultSignUp.data.accountId}`);
 	expect(resultSignUp.data.accountId).toBe(resultOfGetUser.data.account_id);
+	expect(input.name).toBe(resultOfGetUser.data.name)
+	expect(input.email).toBe(resultOfGetUser.data.email)
+	expect(input.cpf).toBe(resultOfGetUser.data.cpf)
+});
+
+test("Deve criar uma conta para o motorista e consultar conta", async function () {
+	const input = {
+		name: "John Doe",
+		email: `john.doe${Math.random()}@gmail.com`,
+		cpf: "87748248800",
+		isPassenger: true,
+		isDriver: true,
+		carPlate: 'AAA1111'
+	};
+	const resultSignUp = await axios.post("http://localhost:3000/signup", input);
+	const resultOfGetUser = await axios.get(`http://localhost:3000/getAccountById/${resultSignUp.data.accountId}`);
+	expect(resultSignUp.data.accountId).toBe(resultOfGetUser.data.account_id);
+	expect(input.name).toBe(resultOfGetUser.data.name)
+	expect(input.email).toBe(resultOfGetUser.data.email)
+	expect(input.cpf).toBe(resultOfGetUser.data.cpf)
 });
