bdde4df287a7c0c257524b19d02021e383acfbc3
Felipe <fcalldas@gmail.com>
2024-04-08 21:46:23 -0300

    ✅ test: update test

--- test/api.test.ts
+++ test/api.test.ts
@@ -19,19 +19,14 @@ test("Deve criar uma conta para o passageiro e consultar conta", async function 
 	expect(input.cpf).toBe(resultOfGetUser.data.cpf)
 });
 
-test("Deve criar uma conta para o motorista e consultar conta", async function () {
+test("Não deve criar uma conta para o passageiro com nome invalido", async function () {
 	const input = {
-		name: "John Doe",
+		name: "John",
 		email: `john.doe${Math.random()}@gmail.com`,
 		cpf: "87748248800",
 		isPassenger: true,
-		isDriver: true,
-		carPlate: 'AAA1111'
 	};
 	const resultSignUp = await axios.post("http://localhost:3000/signup", input);
-	const resultOfGetUser = await axios.get(`http://localhost:3000/getAccountById/${resultSignUp.data.accountId}`);
-	expect(resultSignUp.data.accountId).toBe(resultOfGetUser.data.account_id);
-	expect(input.name).toBe(resultOfGetUser.data.name)
-	expect(input.email).toBe(resultOfGetUser.data.email)
-	expect(input.cpf).toBe(resultOfGetUser.data.cpf)
+	expect(resultSignUp.status).toBe(422)
+	expect(resultSignUp.data).toEqual({"error": "Some error ocurred in signup Error: Invalid name"})
 });
