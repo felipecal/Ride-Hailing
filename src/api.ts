8a7e079f4454efb0068973df4898ed179cc7aec2
Felipe <fcalldas@gmail.com>
2024-04-08 21:06:19 -0300

    🥅 feat: add a new catch

--- src/api.ts
+++ src/api.ts
@@ -7,10 +7,16 @@ app.use(express.json());
 
 
 app.post("/signup", async function (req, res) {
-	const accountDAO = new AccountDAODatabase();
-	const signup = new Signup(accountDAO)
-	const resultOfSignup = await signup.execute(req.body)
-	res.json(resultOfSignup)
+	try {
+		const accountDAO = new AccountDAODatabase();
+		const signup = new Signup(accountDAO)
+		const resultOfSignup = await signup.execute(req.body)
+		res.json(resultOfSignup)
+	} catch (error: any) {
+		res.status(422).json({
+			error: `Some error ocurred in signup ${error}`
+		})
+	}
 });
 
 app.get("/getAccountById/:accountId", async function (req, res) {
