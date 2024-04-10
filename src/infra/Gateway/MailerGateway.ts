e0b4c64879e7c669f002662a23881bdd61ec3063
Felipe <fcalldas@gmail.com>
2024-04-10 19:01:10 -0300

    ♻️ refactor: refactor mailer gateway

--- src/infra/Gateway/MailerGateway.ts
+++ src/infra/Gateway/MailerGateway.ts
@@ -1,8 +1,8 @@
-export default interface MailerGateway {
+export interface MailerGateway {
   send(recipient: string, subject: string, content: string): Promise<void>;
 }
 
-export class MailgerGateway implements MailerGateway {
+export class MailgerGatewayMemory implements MailerGateway {
   async send(recipient: string, subject: string, content: string): Promise<void> {
     console.log(recipient, subject, content);
   }
