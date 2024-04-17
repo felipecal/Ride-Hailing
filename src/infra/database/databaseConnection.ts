e41086a3d7da667d1a6b038b96cbfb7fbac73c32
Felipe <fcalldas@gmail.com>
2024-04-17 20:26:37 -0300

    ✨ feat: add framework and driver

---
+++ src/infra/database/databaseConnection.ts
@@ -0,0 +1,23 @@
+// framework and driver
+
+import pgp from 'pg-promise';
+
+export default interface DatabaseConnection {
+  query(statement: string, params: any): Promise<any>;
+  close(): Promise<void>
+}
+
+export class PgPromiseAdapter implements DatabaseConnection {
+  connection: any;
+
+  constructor() {
+    this.connection = pgp()('cccat16-postgres://postgres:123456@localhost:5432')
+  }
+  
+  query(statement: string, params: any): Promise<any> {
+    return this.connection.query(statement, params);
+  }
+  close(): Promise<void> {
+    return this.connection.close();
+  }
+}
