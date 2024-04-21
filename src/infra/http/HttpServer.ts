95063b9a289526c025e1b2c78c261139c0951871
Felipe <fcalldas@gmail.com>
2024-04-21 20:25:19 -0300

    ♻️ refactor: refactor the code and separate in files

---
+++ src/infra/http/HttpServer.ts
@@ -0,0 +1,31 @@
+// framework and driver
+import express from "express";
+
+export default interface HttpServer {
+  register(method: string, url: string, callback: Function): void;
+  listen(port: number): void
+}
+
+export class ExpressAdapter implements HttpServer {
+  app: any
+
+  constructor() {
+    this.app = express();
+    this.app.use(express.json())
+  }
+
+  register(method: string, url: string, callback: Function) {
+    this.app[method](url.replace(/\{|\}/g, ""), async function (req: any, res: any) {
+      try {
+        const output = await callback(req.params, req.body)
+        res.json(output)
+      } catch (error: any) {
+        res.status(422).json({ message: error.message })
+      }
+    })
+  }
+
+  listen(port: number): void {
+    this.app.listen(port);
+  }
+}
