e851ed81bd059454ecf78df9fa90a6b523e3cf5f
Felipe <fcalldas@gmail.com>
2024-04-21 20:59:41 -0300

    ♻️ refactor: refactor the code

--- src/main_api.ts
+++ src/main_api.ts
@@ -4,12 +4,13 @@
 import { MailerGatewayMemory } from './infra/gateway/MailerGateway';
 import { AccountRepositoryDatabase } from './infra/repository/AccountRepository';
 import { PgPromiseAdapter } from './infra/database/DatabaseConnection';
-import { ExpressAdapter } from './infra/http/HttpServer';
+import { ExpressAdapter, HapiAdapter } from './infra/http/HttpServer';
 import AccountController from './infra/http/AccountController';
 import Signup from './application/usecase/Signup';
 import GetAccount from './application/usecase/GetAccount';
 
 const httpServer = new ExpressAdapter();
+// const httpServer = new HapiAdapter();
 const connection = new PgPromiseAdapter();
 const accountDAO = new AccountRepositoryDatabase(connection);
 const mailerGateway = new MailerGatewayMemory();
@@ -18,18 +19,4 @@ const getAccount = new GetAccount(accountDAO);
 new AccountController(httpServer, signup, getAccount);
 httpServer.listen(3000);
 
-// app.post('/request_ride', async function (req, res) {
-//   try {
-//     const accountDAO = new AccountRepositoryDatabase(connection);
-//     const rideDAO = new RideRepositoryDatabase(connection);
-//     const requestRide = new RequestRide(accountDAO, rideDAO);
-//     const resultOfRequestRide = await requestRide.execute(req.body);
-//     res.json(resultOfRequestRide);
-//   } catch (error) {
-//     res.status(422).json(`Some error ocurred in request_ride ${error}`);
-//   }
-// });
 
-// app.listen(3000, () => {
-//   console.log(`Server running on port 3000 ⛇`);
-// });
