728eccb8b22cc012616778a0f272f8e4e5aa702a
Felipe <fcalldas@gmail.com>
2024-04-23 17:38:29 -0300

    🐛 fix: fix value objects

--- src/domain/vo/Email.ts
+++ src/domain/vo/Email.ts
@@ -2,12 +2,8 @@ export default class Email {
 	private value: string;
 
 	constructor (email: string) {
-		if (this.isInvalidEmail(email)) throw new Error("Invalid email");
+		if (!email.match(/^(.+)@(.+)$/)) throw new Error("Invalid email");
 		this.value = email;
-	}
-
-	private isInvalidEmail (email: string) {
-		return !email.match(/^(.+)@(.+)$/);
 	}
 
 	getValue () {
