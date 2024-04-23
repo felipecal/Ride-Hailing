e65d47eb572974b9cbccb0bc6dbe07c2ef1fc5ad
Felipe <fcalldas@gmail.com>
2024-04-23 17:38:58 -0300

    ✨ feat: add return type

--- src/application/usecase/GetAccount.ts
+++ src/application/usecase/GetAccount.ts
@@ -3,9 +3,27 @@ import { AccountRepository } from '../../infra/repository/AccountRepository';
 export default class GetAccount {
   constructor(readonly accountDAO: AccountRepository) {}
 
-  async execute(accountId: string) {
+  async execute(accountId: string): Promise<Output> {
     const account = await this.accountDAO.getById(accountId);
     if (!account) throw new Error(`Account not found`);
-    return account;
+    return {
+      accountId: account.accountId,
+      name: account.getName(),
+      email: account.getEmail(),
+      cpf: account.getCpf(),
+      carPlate: account.getCarPlate(),
+      isPassenger: account.isPassenger,
+      isDriver: account.isDriver
+    };
   }
+}
+
+type Output = {
+  accountId: string;
+  name: string;
+  email:string;
+  cpf: string;
+  carPlate: string;
+  isPassenger: boolean;
+  isDriver: boolean;
 }
