d5f11e2aaf2b5cafc9dbf1364656961bf7b4a35d
Felipe <fcalldas@gmail.com>
2024-04-25 18:21:12 -0300

    ✨ feat: add value objects in entity

--- src/domain/entity/Ride.ts
+++ src/domain/entity/Ride.ts
@@ -1,25 +1,56 @@
 import crypto from 'crypto';
+import Segment from '../vo/Segment';
+import Coord from '../vo/Coord';
+import RideStatus, { RideStatusFactory } from '../vo/RideStatus';
 
 export default class Ride {
-  private constructor(
-    readonly rideId: string,
-    readonly passengerId: string,
-    readonly fromLat: number,
-    readonly fromLong: number,
-    readonly toLat: number,
-    readonly toLong: number,
-    readonly status: string,
-    readonly date: Date,
-  ) {}
+  status: RideStatus;
+  
+	private constructor (readonly rideId: string, readonly passengerId: string, public driverId: string, private segment: Segment, status: string, readonly date: Date) {
+		this.status = RideStatusFactory.create(this, status);
+	}
 
   static create(passengerId: string, fromLat: number, fromLong: number, toLat: number, toLong: number) {
     const rideId = crypto.randomUUID();
     const status = 'requested';
     const date = new Date();
-    return new Ride(rideId, passengerId, fromLat, fromLong, toLat, toLong, status, date);
+		return new Ride(rideId, passengerId, "", new Segment(new Coord(fromLat, fromLong), new Coord(toLat, toLong)), status, date);
   }
 
-  static restore(rideId: string, passengerId: string, fromLat: number, fromLong: number, toLat: number, toLong: number, status: string, date: Date) {
-    return new Ride(rideId, passengerId, fromLat, fromLong, toLat, toLong, status, date);
+  static restore(rideId: string, passengerId: string, driverId: string, fromLat: number, fromLong: number, toLat: number, toLong: number, status: string, date: Date) {
+		return new Ride(rideId, passengerId, driverId, new Segment(new Coord(fromLat, fromLong), new Coord(toLat, toLong)), status, date);
   }
+
+	accept(driverId: string){
+		this.status.accept();
+		this.driverId = driverId
+	}
+
+	start (){
+		this.status.start()
+	}
+
+	getFromLat () {
+		return this.segment.from.getLat();
+	}
+
+	getFromLong () {
+		return this.segment.from.getLong();
+	}
+
+	getToLat () {
+		return this.segment.to.getLat();
+	}
+
+	getToLong () {
+		return this.segment.to.getLong();
+	}
+
+	getDistance () {
+		return this.segment.getDistance();
+	}
+
+	getStatus () {
+		return this.status.value;
+	}
 }
