06ae539ef81fdc4dbb17ca87015ae8bd2c7cb5ca
Felipe <fcalldas@gmail.com>
2024-04-25 17:33:43 -0300

    ✨ feat: new usecase

---
+++ src/application/usecase/StartRide.ts
@@ -0,0 +1,19 @@
+import { AccountRepository } from "../../infra/repository/AccountRepository";
+import { RideRepository } from "../../infra/repository/RideRepository";
+
+export default class StartRide {
+
+	constructor (readonly rideRepository: RideRepository) {
+	}
+	
+	async execute (input: Input): Promise<void> {
+		const ride = await this.rideRepository.getRideById(input.rideId);
+		ride.start();
+		await this.rideRepository.updateRide(ride);
+	}
+}
+
+//DTO = Data Transfer Object
+type Input = {
+	rideId: string,
+}
