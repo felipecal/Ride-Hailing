172b469f4e8bde63a612755e205134d7bd6faedd
Felipe <fcalldas@gmail.com>
2024-04-26 16:17:43 -0300

    ✨ feat: add new usecase

---
+++ src/application/usecase/UpdatePosition.ts
@@ -0,0 +1,19 @@
+import { PositionRepository } from './../../infra/repository/PositionRepository';
+import Position from "../../domain/entity/Position";
+
+export default class UpdatePosition {
+
+	constructor (private  positionRepository: PositionRepository) {}
+	
+	async execute (input: Input): Promise<void> {
+		const position =  Position.create(input.rideId, input.lat, input.long)
+		this.positionRepository.savePosition(position);
+	}
+}
+
+//DTO = Data Transfer Object
+type Input = {
+	rideId: string,
+	lat: number,
+	long: number
+}
