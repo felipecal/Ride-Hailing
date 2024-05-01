ba030592071cb039f1422218e11b0941e0f7c10c
Felipe <fcalldas@gmail.com>
2024-04-30 21:12:20 -0300

    ✨ feat: add unit of work in connection

--- src/infra/database/databaseConnection.ts
+++ src/infra/database/databaseConnection.ts
@@ -5,6 +5,7 @@ import pgp from 'pg-promise';
 export default interface DatabaseConnection {
   query(statement: string, params: any): Promise<any>;
   close(): Promise<void>;
+  commit(): Promise<void>
 }
 
 export class PgPromiseAdapter implements DatabaseConnection {
@@ -20,4 +21,44 @@ export class PgPromiseAdapter implements DatabaseConnection {
   close(): Promise<void> {
     return this.connection.$pool.end();
   }
+  
+  async commit(): Promise<void> {
+  }
+}
+
+export class UnitOfWork implements DatabaseConnection {
+	connection: any;
+	statements: { statement: string, params: any }[];
+
+	constructor () {
+		this.connection = pgp()("postgres://postgres:123456@localhost:5432/app");
+		this.statements = [];
+	}
+
+	async query(statement: string, params: any, transactional: boolean = false): Promise<any> {
+		if (!transactional) {
+			return this.connection.query(statement, params);
+		} else {
+			this.statements.push({ statement, params });
+		}
+	}
+
+	close(): Promise<void> {
+		return this.connection.$pool.end();
+	}
+
+	async commit(): Promise<void> {
+		await this.connection.tx(async (t: any) => {
+			const transactions = [];
+			for (const statement of this.statements) {
+				transactions.push(await t.query(statement.statement, statement.params));
+			}
+			return t.batch(transactions);
+		}).then((data: any) => {
+			console.log("commit");
+		}).catch((data: any) => {
+			console.log("rollback");
+		});
+	}
+
 }
