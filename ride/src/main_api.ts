d47d091a8031002a4eb812cccab534ebb639ba2a
Felipe <fcalldas@gmail.com>
2024-05-05 20:47:37 -0300

    ♻️ refactor: change the port

--- ride/src/main_api.ts
+++ ride/src/main_api.ts
@@ -4,7 +4,7 @@ import { MailerGatewayMemory } from './infra/gateway/MailerGateway';
 import { AccountRepositoryDatabase } from './infra/repository/AccountRepository';
 import { PgPromiseAdapter } from './infra/database/DatabaseConnection';
 import { ExpressAdapter, HapiAdapter } from './infra/http/HttpServer';
-import AccountController from './infra/http/AccountController';
+// import AccountController from './infra/http/AccountController';
 import Signup from './application/usecase/Signup';
 import GetAccount from './application/usecase/GetAccount';
 
@@ -15,5 +15,5 @@ const accountRepository = new AccountRepositoryDatabase(connection);
 const mailerGateway = new MailerGatewayMemory();
 const signup = new Signup(accountRepository, mailerGateway);
 const getAccount = new GetAccount(accountRepository);
-new AccountController(httpServer, signup, getAccount);
-httpServer.listen(3000);
+// new AccountController(httpServer, signup, getAccount);
+httpServer.listen(3002);
