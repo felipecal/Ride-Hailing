500ca596bfd9c8e501f4af01f906d256ca52f546
Felipe <fcalldas@gmail.com>
2024-05-07 17:42:04 -0300

    🐛 fix: fix a bug in request and refactor

--- ride/src/infra/gateway/AccountGatewayHttp.ts
+++ ride/src/infra/gateway/AccountGatewayHttp.ts
@@ -1,13 +1,15 @@
-import axios from 'axios';
 import AccountGateway from '../../application/gateway/AccountGateway';
+import HttpClient from '../http/HttpClient';
 
 export default class AccountGatewayHttp implements AccountGateway {
+  constructor(readonly httpClient: HttpClient){
+  }
   async getAccount(accountId: string): Promise<{ accountId: string; name: string; email: string; cpf: string; carPlate: string; isPassenger: boolean; isDriver: boolean; }> {
-    const account = await axios.post('http://localhost:3000/signup', accountId);
-    return account.data;
+    const account = await this.httpClient.get(`http://localhost:3000/getAccountById/${accountId}`);
+    return account;
   }
   async signUp(input: { name: string; email: string; cpf: string; carPlate: string; isPassenger: boolean; isDriver: boolean; }): Promise<{ accountId: string; }> {
-    const signup = await axios.post('http://localhost:3000/signup', input);
-    return signup.data
+    const signup = await this.httpClient.post('http://localhost:3000/signup', input);
+    return signup;
   }
 }
