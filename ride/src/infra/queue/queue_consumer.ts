71a3100de9750152469798a82eb0c79d68a707e7
Felipe <fcalldas@gmail.com>
2024-05-09 17:21:06 -0300

    🎨 style: format the file

--- ride/src/infra/queue/queue_consumer.ts
+++ ride/src/infra/queue/queue_consumer.ts
@@ -1,13 +1,12 @@
-import amqp from "amqplib";
+import amqp from 'amqplib';
 
-async function main () {
-	const connection = await amqp.connect("amqp://localhost");
-	const channel = await connection.createChannel();
-	channel.consume("rideCompleted.processPayment", function (msg: any) {
-		const input = JSON.parse(msg.content.toString());
-		console.log(input);
-		channel.ack(msg);
-	});
-
+async function main() {
+  const connection = await amqp.connect('amqp://localhost');
+  const channel = await connection.createChannel();
+  channel.consume('rideCompleted.processPayment', function (msg: any) {
+    const input = JSON.parse(msg.content.toString());
+    console.log(input);
+    channel.ack(msg);
+  });
 }
 main();
