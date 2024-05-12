567c44f968c73d5a29c5752d45d59e12ac02d2e0
Felipe <fcalldas@gmail.com>
2024-05-12 13:54:44 -0300

    🎨 style: format the code

--- payment/src/infra/queue/QueueController.ts
+++ payment/src/infra/queue/QueueController.ts
@@ -1,11 +1,10 @@
-import ProcessPayment from "../../application/usecase/ProcessPayment";
-import Queue from "./Queue";
+import ProcessPayment from '../../application/usecase/ProcessPayment';
+import Queue from './Queue';
 
 export default class QueueController {
-
-	constructor (queue: Queue, processPayment: ProcessPayment) {
-		queue.consume("rideCompleted.processPayment", async (input: any) => {
-			await processPayment.execute(input);
-		});
-	}
+  constructor(queue: Queue, processPayment: ProcessPayment) {
+    queue.consume('rideCompleted.processPayment', async (input: any) => {
+      await processPayment.execute(input);
+    });
+  }
 }
