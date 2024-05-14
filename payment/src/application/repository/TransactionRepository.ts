cc77999ab31b7b7af0172072870fec08503d28b5
Felipe <fcalldas@gmail.com>
2024-05-14 17:47:46 -0300

    ✨ feat: add new feats in payment

---
+++ payment/src/application/repository/TransactionRepository.ts
@@ -0,0 +1,7 @@
+import Transaction from "../../domain/Transaction";
+
+export default interface TransactionRepository{
+  save(transaction: Transaction):Promise<void>;
+  get (transactionId: string): Promise<Transaction>;
+
+}
