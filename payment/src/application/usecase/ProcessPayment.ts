b73d15e603444e7091c498a55267fbb8c835346a
Felipe <fcalldas@gmail.com>
2024-05-17 09:58:50 -0300

    ✨ feat: add feat of payment gateway

--- payment/src/application/usecase/ProcessPayment.ts
+++ payment/src/application/usecase/ProcessPayment.ts
@@ -1,16 +1,33 @@
 import Transaction from '../../domain/Transaction';
+import PaymentGateway from '../gateway/PaymentGateway';
 import TransactionRepository from '../repository/TransactionRepository';
 
 export default class ProcessPayment {
-  constructor(readonly transactionRepository: TransactionRepository) {}
 
-  async execute(input: Input): Promise<Output> {
-    const transaction = Transaction.create(input.rideId, input.amount);
-    await this.transactionRepository.save(transaction);
-    return {
-      transactionId: transaction.transactionId,
-    };
-  }
+	constructor (readonly transactionRepository: TransactionRepository, readonly paymentGateway: PaymentGateway) {
+	}
+	
+	async execute (input: Input): Promise<Output> {
+		console.log("processPayment", input);
+		const transaction = Transaction.create(input.rideId, input.amount);
+		const inputPaymentGateway = {
+			cardHolder: "Cliente Exemplo",
+			creditCardNumber: "4012001037141112",
+			expDate: "05/2027",
+			cvv: "123",
+			amount: input.amount
+		}
+		const outputPaymentGateway = await this.paymentGateway.createTransaction(inputPaymentGateway);
+		if (outputPaymentGateway.status === "approved") {
+			transaction.approve();
+		} else {
+			transaction.reject();
+		}
+		await this.transactionRepository.save(transaction);
+		return {
+			transactionId: transaction.transactionId
+		}
+	}
 }
 
 type Input = {
