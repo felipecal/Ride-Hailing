bc3f9ace52e3ed85c870e1b4caf31f2cc37f8b24
Felipe <fcalldas@gmail.com>
2024-05-17 09:59:03 -0300

    ✅ test: update tes with new feats of payment gatewayt

--- payment/test/ProcessTransaction.test.ts
+++ payment/test/ProcessTransaction.test.ts
@@ -3,13 +3,16 @@ import { PgPromiseAdapter } from '../src/infra/database/DatabaseConnection';
 import crypto from 'crypto';
 import TransactionRepositoryDatabase from '../src/infra/repository/TransactionRepositoryDatabase';
 import GetTransaction from '../src/application/usecase/GetTransaction';
+import PJBankGateway from '../src/infra/gateway/PJBankGateway';
+import ORM from '../src/infra/orm/ORM';
+import TransactionRepositoryORM from '../src/infra/repository/TransactionRepositoryORM';
 
 test('Deve criar uma transação', async function () {
   const connection = new PgPromiseAdapter();
-  const transactionRepository = new TransactionRepositoryDatabase(connection);
-  // const orm = new ORM(connection);
-  // const transactionRepository = new TransactionRepositoryORM(connection);
-  const processPayment = new ProcessPayment(transactionRepository);
+  // const transactionRepository = new TransactionRepositoryDatabase(connection);
+  const orm = new ORM(connection);
+  const transactionRepository = new TransactionRepositoryORM(orm);
+  const processPayment = new ProcessPayment(transactionRepository, new PJBankGateway());
   const inputProcessPayment = {
     rideId: crypto.randomUUID(),
     amount: 100,
@@ -18,6 +21,6 @@ test('Deve criar uma transação', async function () {
   const getTransaction = new GetTransaction(transactionRepository);
   const outputGetTransaction = await getTransaction.execute(outputProcessPayment.transactionId);
   expect(outputGetTransaction.amount).toBe(100);
-  expect(outputGetTransaction.status).toBe('pending');
+  expect(outputGetTransaction.status).toBe('approved');
   await connection.close();
 });
