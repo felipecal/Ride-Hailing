519ea9a4b1d9a2283887ae5959744943992d7458
Felipe <fcalldas@gmail.com>
2024-05-19 20:38:48 -0300

    🎨 style: format payment file

--- payment/src/application/gateway/PaymentGateway.ts
+++ payment/src/application/gateway/PaymentGateway.ts
@@ -1,17 +1,17 @@
 export default interface PaymentGateway {
-	createTransaction(input: Input): Promise<Output>;
+  createTransaction(input: Input): Promise<Output>;
 }
 
 type Input = {
-	creditCardNumber: string,
-	cardHolder: string,
-	expDate: string,
-	cvv: string,
-	amount: number
-}
+  creditCardNumber: string;
+  cardHolder: string;
+  expDate: string;
+  cvv: string;
+  amount: number;
+};
 
 type Output = {
-	tid: string,
-	authorizationCode: string,
-	status: string
-}
+  tid: string;
+  authorizationCode: string;
+  status: string;
+};
